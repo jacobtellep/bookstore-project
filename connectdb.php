79f3467285d2113e45d963a839e4fcd2df33b3fa
Jacob Tellep <jj.tellep@gmail.com>
2021-04-18 13:56:24 -0400

    added login files. Haven't done anything with them yet.

--- connectdb.php
+++ connectdb.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqli = mysqli_connect("localhost", "jtellep", "jtellep", "jtellep");
+$mysqli = mysqli_connect("localhost", "root", "", "bookstore", "3309");
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
